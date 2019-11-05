9c887e1ee92c1fc49218d7f5ea26869511b8dbb6
Roman Zvyazok <roman.zvyazok@eleks.com>
2019-11-05 13:03:18 +0200

    Updates

---
+++ precache-manifest.fec7797f4e54c36658d09e838d3f886b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ce167b29f800d0c432051e97a91c05",
+    "url": "git%20remote%20add%20origin%20https://%7Busername%7D:%7Bpassword%7D@github.com/%7Busername%7D/project.git/index.html"
+  },
+  {
+    "revision": "ce71c8d75105471d5ad5",
+    "url": "git%20remote%20add%20origin%20https://%7Busername%7D:%7Bpassword%7D@github.com/%7Busername%7D/project.git/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c82fd00a81e851e93ae6",
+    "url": "git%20remote%20add%20origin%20https://%7Busername%7D:%7Bpassword%7D@github.com/%7Busername%7D/project.git/static/css/main.65605878.chunk.css"
+  },
+  {
+    "revision": "ce71c8d75105471d5ad5",
+    "url": "git%20remote%20add%20origin%20https://%7Busername%7D:%7Bpassword%7D@github.com/%7Busername%7D/project.git/static/js/2.74abff53.chunk.js"
+  },
+  {
+    "revision": "c82fd00a81e851e93ae6",
+    "url": "git%20remote%20add%20origin%20https://%7Busername%7D:%7Bpassword%7D@github.com/%7Busername%7D/project.git/static/js/main.656d2d31.chunk.js"
+  },
+  {
+    "revision": "67a0536edf8d065a7ba4",
+    "url": "git%20remote%20add%20origin%20https://%7Busername%7D:%7Bpassword%7D@github.com/%7Busername%7D/project.git/static/js/runtime-main.c17a62cd.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "git%20remote%20add%20origin%20https://%7Busername%7D:%7Bpassword%7D@github.com/%7Busername%7D/project.git/static/media/bg.b6796c73.jpg"
+  }
+]);
